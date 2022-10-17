421d3744af2a7331b8e7335692a5328d8420a132
Curtis <curtischung06@gmail.com>
2022-10-16 23:52:52 -0400

    log in and sign up modal completed

---
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import * as sessionActions from '../../store/session';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import '../LoginFormPage/LoginForm.css';
+
+function LoginForm() {
+    const dispatch = useDispatch();
+    const sessionUser = useSelector(state => state.session.user);
+    const [credential, setCredential] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState([]);
+
+    if (sessionUser) return (
+        <Redirect to="/" />
+    );
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setErrors([]);
+        return dispatch(sessionActions.login({ credential, password }))
+            .catch(async (res) => {
+                const data = await res.json();
+                if (data && data.errors) setErrors(data.errors);
+            });
+    }
+
+    return (
+        <div className='container'>
+            <div className="container-header">
+                <button className='close-button'>x</button>
+                <div className="log-in-or-sign-up">Log in or sign up</div>
+            </div>
+            <div className="container-body">
+                <form onSubmit={handleSubmit} className="form-box">
+                    <div className="container-body-content">
+                        <div className="welcome">Welcome to Be-myguest</div>
+                        <div className='inputs'>
+                            <div>
+                                <label>
+                                    <input
+                                        type="text"
+                                        value={credential}
+                                        onChange={(e) => setCredential(e.target.value)}
+                                        required
+                                        placeholder="Username or email"
+                                        className="input-fields-1"
+                                    />
+                                </label>
+                            </div>
+                            <div className='line'></div>
+                            <div>
+                                <label>
+                                    <input
+                                        type="password"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        required
+                                        placeholder='Password'
+                                        className="input-fields-2"
+                                    />
+                                </label>
+                            </div>
+                        </div>
+                        <ul>
+                            {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                        </ul>
+                        <div>
+                            <button type="submit" className="login-button">Log In</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default LoginForm;
---
+++ frontend/src/components/SignupFormModal/SignupForm.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import * as sessionActions from "../../store/session";
+import "../SignupFormPage/SignupForm.css"
+
+function SignupForm() {
+    const dispatch = useDispatch();
+    const sessionUser = useSelector((state) => state.session.user);
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [errors, setErrors] = useState([]);
+
+    if (sessionUser) return <Redirect to="/" />;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (password === confirmPassword) {
+            setErrors([]);
+            return dispatch(sessionActions.signup({ email, username, password }))
+                .catch(async (res) => {
+                    const data = await res.json();
+                    if (data && data.errors) setErrors(data.errors);
+                });
+        }
+        return setErrors(['Confirm Password field must be the same as the Password field']);
+    };
+
+    return (
+        <div className='sign-up-container'>
+            <div className="sign-up-container-header">
+                <button className='close-button'>x</button>
+                <div className="sign-up-for-bemyguest">Log in or sign up</div>
+            </div>
+            <div className="sign-up-container-body">
+                <form onSubmit={handleSubmit} className="sign-up-form-box">
+                    <div className="sign-up-container-body-content">
+                        <div className="welcome">Welcome to Be-myguest</div>
+                        <div className='sign-up-inputs'>
+                            <div>
+                                <label>
+                                    <input
+                                        type="text"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        required
+                                        placeholder="Email"
+                                        className="sign-up-input-fields-1"
+                                    />
+                                </label>
+                            </div>
+                            <div className='line'></div>
+                            <div>
+                                <label>
+                                    <input
+                                        type="text"
+                                        value={username}
+                                        onChange={(e) => setUsername(e.target.value)}
+                                        required
+                                        placeholder="Username"
+                                        className="sign-up-input-fields-2"
+                                    />
+                                </label>
+                            </div>
+                            <div className='line'></div>
+                            <div>
+                                <label>
+                                    <input
+                                        type="password"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        required
+                                        placeholder="Password"
+                                        className="sign-up-input-fields-3"
+                                    />
+                                </label>
+                            </div>
+                            <div className='line'></div>
+                            <div>
+                                <label>
+                                    <input
+                                        type="password"
+                                        value={confirmPassword}
+                                        onChange={(e) => setConfirmPassword(e.target.value)}
+                                        required
+                                        placeholder="Confirm Password"
+                                        className="sign-up-input-fields-4"
+                                    />
+                                </label>
+                            </div>
+                        </div>
+                        <ul>
+                            {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                        </ul>
+                        <div>
+                            <button type="submit" className="sign-up-signup-button">Sign Up</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default SignupForm;
